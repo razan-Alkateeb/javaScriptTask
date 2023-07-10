8a15b09429d049b0192a304082bacefc9a484734
razan alkateeb <razan.alkateeb0@gmail.com>
2023-07-10 09:07:54 +0300

    added

---
+++ tasks9July/jsTask1.js
@@ -0,0 +1,120 @@
+/*
+let x = 250 ;
+create variable Calculate the value of zakat for x ,If you know the percentage of zakat = 2.5 %; 
+*/
+
+// let x = 250 ;
+// let percentage = 2.5;
+// let zakat = (percentage/100) * x;
+// console.log(`Zakat value for ${x} is ${zakat}`);
+
+
+
+
+//  let arrayOne = [ 1,7,9,45];
+//  console.log(arrayOne);
+
+//  let arrayTwo = ["Str", "alex","moh"];
+//  console.log(arrayTwo);
+
+//  let arrayThree = ['the','fox' ,'over', "lazy", 'dog' ];
+//  console.log(arrayThree);
+
+
+
+
+
+//What the index of "Banana","Tomato"
+// var fruits=["Tomato","Banana","Watermelon"];
+// console.log(fruits[1]);
+// console.log(fruits[0]);
+
+
+
+
+
+
+//Create an array represents your:
+// let favoriteFood =["pasta" ,"pizza" ];
+// console.log(favoriteFood);
+
+// let favoriteSport =["Running" , "Football" , "swmming"];
+// console.log(favoriteSport);
+
+// let favoriteMovie =["TheGodfather" ,"SpaceOdyssey"];
+// console.log(favoriteMovie);
+
+
+
+
+//Create a Variable to return the first element in an array 
+//Ex: firstOfArray([1,4,5]) => 1
+//Ex: firstOfArray(["t","u","g","x"]) => "t"
+
+// let listOne = [1,4,5];
+// console.log(listOne[0]);
+// console.log(listOne[listOne.length-1]);
+
+
+// let listTwo = ["t","u","g","x"];
+// console.log(listTwo[0]);
+// console.log(listTwo[listTwo.length-1]);
+
+
+
+
+
+
+//Create a Variable to return the lastOfArray element in an array 
+// Ex: lastOfArray([1,4,5]) => 5
+// Ex: lastOfArray(["t","u","g","x"]) => "x"
+
+
+
+/*
+6-Using console make this array to be like this one (push, unshift, shift, pop)
+var array = [0,5,7,9]
+=> [1,3,4,6,8,9,10]
+*/
+
+// let dyarray = [0,5,7,9];
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.unshift(1,3,4,6,8);
+// dyarray.push(10);
+
+// console.log(dyarray);
+
+
+// dyarray.pop();
+// dyarray.push(-7);
+// dyarray.push(3.5);
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.shift();
+// dyarray.unshift(5);
+// console.log(dyarray);
+
+
+/*
+7-Using the console try to figure out what the thing thats (push, unshift, shift, pop) return to you
+var array2 = [5,9,-7,3.5]
+*/
+
+
+
+
+
+
+/*
+8.Write a JavaScript program to sort the items of an array.
+Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8
+*/
+
+// var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// console.log(arr1);
+// console.log(arr1.sort());
---
+++ tasks9July/main.js
@@ -0,0 +1,141 @@
+
+// var x = 5;
+// var x = 6;
+// console.log(x);
+
+// let x = 5;
+// x = 6;
+// console.log(x);
+
+
+// const p = 5;
+//  p = 6;
+//  console.log(p);
+
+// let year=2023;
+// let divTest=` <div class="card">
+// <h1>javaScript ${year}<h1>
+//  </div>`;
+// document.write(divTest);
+
+//string--------------------------------
+
+// let text="i like javascript";
+// console.log(text[3]);
+// console.log(text.charAt[3]);
+// console.log(text.length);
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.indexOf("java"));
+// console.log(text.lastIndexOf(2));
+// console.log(text.repeat(4));
+// console.log(text.split(" "));
+// console.log(text.includes("java"));
+// console.log(text.startsWith("i"));
+// console.log(text.endsWith("t"));
+
+
+// //(start,end)/accepted negative number (start from end)
+// console.log(text.slice(-5,-2));
+
+// //(start,end)/not accepted negative number
+// console.log(text.substring(3,7));//(3,7)===(7,3)
+
+// //(strat,length)/(negative number start from end)
+// console.log(text.substr(-6));
+
+
+//object---------------------------------
+
+// let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// console.log(person);
+// console.log(person.age);
+
+//Date----------------------------
+
+// const d = new Date("2023-09-7");
+
+// const date=new Date();
+// const day=date.getDay();
+// const hours=date.getHours();
+// const year=date.getFullYear();
+
+// console.log(day);
+// console.log(hours);
+// console.log(year);
+
+// Array-----------------------------------
+
+// let nameStudents = ["ahmad" ,"alaa", "mohammed" , "ibrahim" ];
+
+// console.log(nameStudents);
+// console.log(typeof nameStudents);
+
+// console.log(`The Students ${nameStudents}`);
+
+// console.log(`Hello ${nameStudents[3]+3}`);
+
+// nameStudents[4]="Ali";
+
+// nameStudents[5]=[1,2];
+// console.log(nameStudents);
+// console.log(nameStudents[5][1]);
+// let x=1;
+// console.log(Array.isArray(x));
+
+// console.log(nameStudents.length);
+// nameStudents[nameStudents.length]="Ali";
+// console.log(nameStudents);
+// nameStudents.length=3;
+
+// //add first
+// console.log(nameStudents);
+// nameStudents.unshift("razan");
+// //add last
+// nameStudents.push("sara");
+
+// //remove first
+// nameStudents.shift();
+// //remove last
+// nameStudents.pop();
+
+// console.log(nameStudents.indexOf("Alaa"));
+
+// console.log(nameStudents.includes("Ahmad"));
+
+// console.log(nameStudents.sort());
+// console.log(nameStudents.reverse());
+
+// let newStudents =["rama","dana"];
+// let allStudents=nameStudents.concat(newStudents);
+
+// console.log(allStudents);
+
+
+//Type Conversion------------------
+
+// console.log(+"80");
+// console.log(+oxff);
+
+// console.log(-"80");
+// console.log(-"-80");
+
+// console.log((99).toString());
+// console.log(99..toString());
+// console.log(99.5.toString());
+
+// console.log(95.555555.toFixed(2));
+
+// console.log(parseInt("90"));
+// console.log(parseInt("90 days"));
+// console.log(parseFloat("100.69 jd"));
+
+// console.log(Number.isInteger("80"));
+// console.log(Number.isInteger(80));
+
+
+
+
+
+
+
